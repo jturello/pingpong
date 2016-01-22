3f484788e3c1568d73e7e833ebe0017d93db08dd
James Turello <jdturello@gmail.com>
2016-01-22 09:30:49 -0800

    add 2nd failing test for 3 returns 'ping' condition

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe('pingpong', function() {
   it("displays list of numbers from 1 to the number the user enters", function() {
-    expect(pingpong(3)).to.equal('1,2,3');
+    expect(pingpong(2)).to.equal('1,2');
+  });
+
+  it("displays 'ping' in place of any number in the generatedNumberList that is a multiple of 3", function() {
+    expect(pingpong(3)).to.equal('1,2,ping');
   });
 });
