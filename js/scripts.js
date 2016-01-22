1d188939f1e8030e0bcb00b8294fadf72d56f64b
James Turello <jdturello@gmail.com>
2016-01-22 09:41:41 -0800

    add script code for 2nd passing spec for 3 returns 'ping' condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 var pingpong = function(userNumber) {
 
     var generatedNumberList = [];
-    
+
     for(var i=1; i <= userNumber; i++){
-      generatedNumberList.push(i);
+      alert("i: " + i + ", userNumber: " + userNumber + ", userNumber%3: " + userNumber%3);
+      if (i % 3 === 0){
+        generatedNumberList.push('ping');
+      } else {
+        generatedNumberList.push(i);
+      }
     }
     return generatedNumberList.join();
   };
