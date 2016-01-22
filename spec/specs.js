a66a255afe7cf7d451fea893bebc09a117f1984f
James Turello <jdturello@gmail.com>
2016-01-22 12:45:41 -0800

    refactor text in specs to be more accurate and user friendly

--- spec/specs.js
+++ spec/specs.js
@@ -3,19 +3,19 @@ describe('pingpong', function() {
     expect(pingpong(2)).to.equal('1,2');
   });
 
-  it("displays 'ping' in place of numbers divisible by 3 in generatedNumberList", function() {
+  it("displays 'ping' in place of numbers divisible by 3 but not also by 15", function() {
     expect(pingpong(3)).to.equal('1,2,ping');
   });
 
-  it("returns 'pong' in place of numbers divisible by 5 in generatedNumberList", function() {
+  it("returns 'pong' in place of numbers divisible by 5 but not also by 15", function() {
     expect(pingpong(5)).to.equal('1,2,ping,4,pong');
   });
 
-  it("returns 'pingpong' in place of numbers divisible by 15 in generatedNumberList", function(){
+  it("returns 'pingpong' in place of numbers divisible by 15", function(){
     expect(pingpong(15)).to.equal('1,2,ping,4,pong,ping,7,8,ping,pong,11,ping,13,14,pingpong');
   });
 
-  it("returns error message 'Invalid entry. Please enter positive integers only.'", function(){
+  it("returns error message 'Invalid entry. Please enter positive integers only.' when the user enters negative numbers", function(){
     expect(pingpong(-1)).to.equal('Invalid entry. Please enter positive integers only.');
   });
 
@@ -23,7 +23,7 @@ describe('pingpong', function() {
     expect(pingpong(3.222)).to.equal('1,2,ping');
   });
 
-  it("returns error message 'Invalid entry. Please enter positive integers only.'", function(){
+  it("returns error message 'Invalid entry. Please enter positive integers only.' when the user enters a string", function(){
     expect(pingpong('string')).to.equal('Invalid entry. Please enter positive integers only.');
   });
 
